e5216b83940fd05e57fd6c42434942408c920dbf
Kristofor Carle <kristoforcarle@moabi.org>
2016-05-26 20:04:59 -0400

    update travis-ci config to use ssh deploy key

--- .build_scripts/deploy.sh
+++ .build_scripts/deploy.sh
@@ -4,13 +4,31 @@ set -e # halt script on error
 # If this is the deploy branch, push it up to gh-pages
 if [ $TRAVIS_PULL_REQUEST = "false" ] && [ $TRAVIS_BRANCH = ${DEPLOY_BRANCH} ]; then
   echo "Get ready, we're zipping and pushing!"
+
+  REPO=`git config remote.origin.url`
+  SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+  SHA=`git rev-parse --verify HEAD`
+  
   cd deploy
   git init
   git config user.name "Travis-CI"
   git config user.email "travis@somewhere.com"
   git add .
-  git commit -m "CI deploy to gh-pages"
-  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+  git commit -m "Deploy to GitHub Pages: ${SHA}"
+
+  # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+  ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+  ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+  ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+  ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+  openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+  chmod 600 deploy_key
+  eval `ssh-agent -s`
+  ssh-add deploy_key
+
+  # Now that we're all set up, we can push.
+  git push --force --quiet $SSH_REPO master:gh-pages
+  #git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 else
   echo "Not a publishable branch so we're all done here"
 fi
