b47e249ea56e7a15390d398718c8b5c913332c57
Kristofor Carle <kristoforcarle@moabi.org>
2016-05-27 14:56:47 -0400

    fix error in josm metric when using planet stream simulation

--- src/metrics/josm_edits.js
+++ src/metrics/josm_edits.js
@@ -1,4 +1,7 @@
 // Returns 1 if JOSM was used, 0 otherwise
 module.exports = function (editorStr) {
-  return editorStr.toLowerCase().match(/^josm/) ? 1 : 0;
+  if (editorStr) {
+    return editorStr.toLowerCase().match(/^josm/) ? 1 : 0;
+  }
+  return 0;
 };
